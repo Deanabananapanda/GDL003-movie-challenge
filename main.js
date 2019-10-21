a5f3a5635c29009725fce65fdb486cfb9e447a8c
dianaglezlag@gmail.com <dianaglezlag@gmail.com>
2019-10-21 15:16:22 -0500

    actualizado

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert("hola");
+//function showData(myJson){
+    //let name = myJson.Title;
+    //let textnode = document.createTextNode(name); 
+    //document.getElementById("datos_ghibli").appendChild(textnode);
+   //}
+   
+
+   //let bringData = () => {
+   //fetch("http://www.omdbapi.com/?t=Spirited+Away&apikey=5a71d114")
+    //.then(function(response){
+      //return response.json();
+    //})
+    //.then(function(myJson){
+      //showData(myJson)
+      //console.log(myJson);
+    //});
+   
+   //}
+
+   
